9b718b07064323b06953c277a13ee80245a4c6c8
dsucksto <dsucksto@thoughtworks.com>
2015-06-26 15:09:15 -0500

    Java7 IntegerReporter: generify predicate

--- java7/src/main/java/com/thoughtworks/jcprogram/functional/exercise/Predicate/IsGreaterThanFour.java
+++ java7/src/main/java/com/thoughtworks/jcprogram/functional/exercise/Predicate/IsGreaterThanFour.java
@@ -3,9 +3,9 @@ package com.thoughtworks.jcprogram.functional.exercise.Predicate;
 /**
  * Created by dsucksto on 6/26/15.
  */
-public class IsGreaterThanFour implements Predicate {
+public class IsGreaterThanFour<F> implements Predicate {
     @Override
-    public boolean isTrue(Integer integer) {
-        return integer > 4;
+    public boolean isTrue(Object object) {
+        return 4 < (int) object;
     }
 }
--- java7/src/main/java/com/thoughtworks/jcprogram/functional/exercise/Predicate/Predicate.java
+++ java7/src/main/java/com/thoughtworks/jcprogram/functional/exercise/Predicate/Predicate.java
@@ -3,6 +3,6 @@ package com.thoughtworks.jcprogram.functional.exercise.Predicate;
 /**
  * Created by dsucksto on 6/26/15.
  */
-public interface Predicate {
-    boolean isTrue(Integer integer);
+public interface Predicate<F> {
+    boolean isTrue(F fInstance);
 }
