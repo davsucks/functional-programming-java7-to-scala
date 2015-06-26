b74d47207d3edb4931cbf9da1dc69ab88000e3d5
dsucksto <dsucksto@thoughtworks.com>
2015-06-26 16:25:49 -0500

    Groovy is sexy

--- groovy/src/main/groovy/com/thoughtworks/jcprogram/functional/exercise/IntegerReporter.groovy
+++ groovy/src/main/groovy/com/thoughtworks/jcprogram/functional/exercise/IntegerReporter.groovy
@@ -1,5 +1,7 @@
 package com.thoughtworks.jcprogram.functional.exercise
 
+import static java.lang.Math.sqrt
+
 // Given a list of numbers like {1, 9, 4, 16, 4}
 // Print the square root of all of the numbers larger than 4.
 // For example, "3, 4"
@@ -10,5 +12,5 @@ println("------------ Groovy: IntegerReporter ------------")
 println new IntegerReporter().reportSquareRootsOfLargeNumbers(numbers)
 
 def reportSquareRootsOfLargeNumbers(numbers){
-    ""
+    numbers.findAll {cat -> cat > 4}.collect {cat -> (int) sqrt(cat)}.join(", ")
 }
